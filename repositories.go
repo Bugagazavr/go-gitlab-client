88fea9d03b786ab078df162cd1cacdabcc1c2291
Christoph Pech <christoph.pech@gmail.com>
2015-06-05 14:04:59 +0200

    tree

--- repositories.go
+++ repositories.go
@@ -53,6 +53,15 @@ type Commit struct {
 	CreatedAt    time.Time
 }
 
+type File struct {
+	Id   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Type string `json:"type,omitempty"`
+	Mode string `json:"mode,omitempty"`
+
+	Children []*File
+}
+
 /*
 Get a list of repository branches from a project, sorted by name alphabetically.
 
@@ -206,4 +215,18 @@ func (g *Gitlab) RepoRawFile(id, sha, filepath string) ([]byte, error) {
 	contents, err := g.buildAndExecRequestRaw("GET", url_, opaque, nil)
 
 	return contents, err
+}
+
+func (g *Gitlab) RepoTree(id, ref, path string) ([]*File, error) {
+
+	url, opaque := g.ResourceUrlRaw(repo_url_tree, map[string]string{":id": id, ":ref": ref, ":path": path})
+
+	var files []*File
+
+	contents, err := g.buildAndExecRequestRaw("GET", url, opaque, nil)
+	if err == nil {
+		err = json.Unmarshal(contents, &files)
+	}
+
+	return files, err
 }
